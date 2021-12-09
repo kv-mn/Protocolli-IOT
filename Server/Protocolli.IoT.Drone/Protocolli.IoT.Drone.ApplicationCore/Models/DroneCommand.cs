d006d9a1aa6aa7076a2faece475e3b42a743b7c4
Kevin Mainardis <kevin.mainardis@stud.tecnicosuperiorekennedy.it>
2021-12-09 16:45:40 +0100

    Drone commands model

---
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.ApplicationCore/Models/DroneCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocolli.IoT.Drone.ApplicationCore.Models
+{
+    public class DroneCommand
+    {
+        public int DroneId { get; set; }
+        public string Command { get; set; }
+        public bool Value { get; set; }
+        public long TimeStamp { get; set; }
+    }
+}

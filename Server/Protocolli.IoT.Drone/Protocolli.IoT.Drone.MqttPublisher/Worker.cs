634ff24976ee5cec710d3132956f42a9de0b227a
Kevin Mainardis <kevin.mainardis@stud.tecnicosuperiorekennedy.it>
2021-12-14 00:26:02 +0100

    Implemented DroneCommand publishing

--- Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.MqttPublisher/Worker.cs
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.MqttPublisher/Worker.cs
@@ -1,20 +1,43 @@
+using MQTTnet.Client.Receiving;
+using Protocolli.IoT.Drone.ApplicationCore.Models;
+using Protocolli.IoT.Drone.Infrastructure.Messaging;
+using System.Text.Json;
+
 namespace Protocolli.IoT.Drone.MqttPublisher
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly MqttClient _mqttClient;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, MqttClient mqttClient)
         {
             _logger = logger;
+            _mqttClient = mqttClient;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            _logger.LogInformation("Worker running at: {time}", DateTime.Now);
+            await _mqttClient.ConnectAsync();
+
+            const int droneCount = 3;
+
+            while (true)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                for (int i = 0; i < droneCount; i++)
+                {
+                    var droneCommand = DroneCommand.RandomDroneCommand(i);
+                    var payload = JsonSerializer.Serialize(droneCommand);
+                    var topic = $"gameofdrones/{droneCommand.DroneId}/commands";
+                    var qos = 2;
+
+                    await _mqttClient.PublishAsync(topic, payload, qos: qos);
+
+                    _logger.LogInformation($"{DateTime.Now} | Publish | Topic: {topic} | QoS: {qos}");
+                }
+
+                Thread.Sleep(2000);
             }
         }
     }

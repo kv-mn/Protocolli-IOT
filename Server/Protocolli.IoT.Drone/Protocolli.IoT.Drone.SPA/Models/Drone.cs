271ad449327e3384b4a14e0b25a8405553086ae1
Kevin Mainardis <kevin.mainardis@stud.tecnicosuperiorekennedy.it>
2021-12-16 00:52:56 +0100

    Dashboard first draft

---
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.SPA/Models/Drone.cs
@@ -0,0 +1,10 @@
+﻿using Protocolli.IoT.Drone.ApplicationCore.Models;
+
+namespace Protocolli.IoT.Drone.SPA.Models
+{
+    public class Drone
+    {
+        public int DroneId { get; set; }
+        public DroneStatus LastStatus { get; set; }
+    }
+}

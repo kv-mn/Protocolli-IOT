341365719333ae960c5694cdfcc91a2c9bcc09a3
Kevin Mainardis <kevin.mainardis@stud.tecnicosuperiorekennedy.it>
2021-12-15 19:58:42 +0100

    Retain flag for commands publishing

--- Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.MqttPublisher/Worker.cs
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.MqttPublisher/Worker.cs
@@ -32,7 +32,7 @@ namespace Protocolli.IoT.Drone.MqttPublisher
                     var topic = $"gameofdrones/{droneCommand.DroneId}/commands";
                     var qos = 2;
 
-                    await _mqttClient.PublishAsync(topic, payload, qos: qos);
+                    await _mqttClient.PublishAsync(topic, payload, qos, true);
 
                     _logger.LogInformation($"{DateTime.Now} | Publish | Topic: {topic} | QoS: {qos}");
                 }

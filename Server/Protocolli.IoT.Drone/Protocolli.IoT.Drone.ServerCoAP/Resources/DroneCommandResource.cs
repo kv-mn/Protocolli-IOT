04eebb461cb8520ed07a0ae72c471df405919e46
Kevin Mainardis <kevin.mainardis@stud.tecnicosuperiorekennedy.it>
2022-01-10 00:27:57 +0100

    wip: commands resource

---
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.ServerCoAP/Resources/DroneCommandResource.cs
@@ -0,0 +1,11 @@
+﻿using CoAP.Server.Resources;
+
+namespace Protocolli.IoT.Drone.ServerCoAP.Resources
+{
+    internal class DroneCommandResource : Resource
+    {
+        public DroneCommandResource(string name) : base(name)
+        {
+        }
+    }
+}
